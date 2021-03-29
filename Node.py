4a2b3365acaeb0a3e0e94689e2aca7a601413c26
HermanLin <hl3213@nyu.edu>
2021-03-29 17:58:04 -0400

    typecast value to string for comparisons

--- Node.py
+++ Node.py
@@ -14,16 +14,22 @@ class Node:
     def getState(self):
         return copy.deepcopy(self._state)
 
+    def getParent(self):
+        return self._parent
+
+    def getAction(self):
+        return self._action
+
     def getDepth(self):
         return self._depth
 
     def getCost(self):
         if not self._cost: # calculate if the cost has not been set
-            self._cost = self.getDepth() + self._heuristic.calc_SCBD(self)
+            self._cost = self._depth + self._heuristic.calc_SCBD(self)
         return self._cost
 
     def getValuePos(self, value):
         for i in range(4):
             for j in range(4):
-                if self._state[i][j] == value:
+                if self._state[i][j] == str(value):
                     return i, j
