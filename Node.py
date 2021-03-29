02214fe7d652555baef0acf95cd2beb53a81a44c
HermanLin <hl3213@nyu.edu>
2021-03-29 13:16:26 -0400

    added getCost method

--- Node.py
+++ Node.py
@@ -4,9 +4,12 @@ class Node:
 
     def __init__(self, state, parent, action, cost):
         self._state = state
-        self._parent = None
-        self._action = None
-        self._cost = None
+        self._parent = parent
+        self._action = action
+        self._cost = cost
+
+    def getCost(self):
+        return self._cost
 
     def getValuePos(self, value):
         for i in range(4):
