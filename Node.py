6d6b8bbd753f6f7ceb4baf7eb3e01a5ecb0a9022
HermanLin <hl3213@nyu.edu>
2021-03-28 22:17:42 -0400

    implemented value position finder function

--- Node.py
+++ Node.py
@@ -3,5 +3,12 @@
 class Node:
 
     def __init__(self, state, moves):
-        self._state = state
+        self._state = state # always 4 by 4
         self._moves = moves    
+
+    def getValuePos(self, value):
+        for i in range(4):
+            for j in range(4):
+                if self._state[i][j] == value:
+                    return i, j
+                    
