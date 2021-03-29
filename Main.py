1d39e118a79454fbd60b8a2c7d997b8b0d5728e2
HermanLin <hl3213@nyu.edu>
2021-03-29 09:49:29 -0400

    implemented Node class into processFile

--- Main.py
+++ Main.py
@@ -1,18 +1,23 @@
 from shutil import copyfile
 from Node import Node
+from SumSCBD import SumSCBD
 
 # read and process an input file for the initial and goal states
 def processFile (filename):
     f = open(filename, 'r')
     # read 4 lines and remove trailing newlines for initial and goal
-    initial = [next(f).rstrip() for line in range(4)]
+    initialState = [next(f).rstrip() for line in range(4)]
     next(f) # skip blank line
-    goal = [next(f).rstrip() for line in range(4)]
+    goalState = [next(f).rstrip() for line in range(4)]
     f.close()
 
     # turn into 2d lists
-    initial = [row.split(' ') for row in initial]
-    goal = [row.split(' ') for row in goal]
+    initialState = [row.split(' ') for row in initial]
+    goalState = [row.split(' ') for row in goal]
+
+    # initial cost should be calculated
+    initial = Node(initialState, None, None, None) 
+    goal = SumSCBD(goalState)
 
     return initial, goal
 
