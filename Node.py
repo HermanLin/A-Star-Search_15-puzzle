124f6711426117ebbc14b1bd0a2b2b2962514edd
HermanLin <hl3213@nyu.edu>
2021-03-29 14:10:22 -0400

    implemented getState method

--- Node.py
+++ Node.py
@@ -1,12 +1,16 @@
-# Node class for A-Star Search Algorithm
+import copy 
 
 class Node:
+    # Node class for A-Star Search Algorithm
 
     def __init__(self, state, parent, action, cost):
         self._state = state
         self._parent = parent
         self._action = action
         self._cost = cost
+
+    def getState(self):
+        return copy.deepcopy(self._state)
 
     def getCost(self):
         return self._cost
