d04ddcc392f9afb515769782ac1fe3ab3c133291
HermanLin <hl3213@nyu.edu>
2021-03-29 15:36:02 -0400

    added self cost calculation functionality

--- Node.py
+++ Node.py
@@ -3,16 +3,19 @@ import copy
 class Node:
     # Node class for A-Star Search Algorithm
 
-    def __init__(self, state, parent, action, cost):
+    def __init__(self, state, parent, action, heuristic):
         self._state = state
         self._parent = parent
         self._action = action
-        self._cost = cost
+        self._heuristic = heuristic
+        self._cost = None
 
     def getState(self):
         return copy.deepcopy(self._state)
 
     def getCost(self):
+        if not self._cost: # calculate if the cost has not been set
+            self._cost = self._heuristic.calc_SCBD(self)
         return self._cost
 
     def getValuePos(self, value):
