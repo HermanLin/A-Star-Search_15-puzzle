9d7c4bf0508f40a36715c0fce2b46d1f6f26c350
HermanLin <hl3213@nyu.edu>
2021-03-28 22:55:00 -0400

    added get function for moves list

--- Node.py
+++ Node.py
@@ -3,13 +3,12 @@
 class Node:
 
     def __init__(self, state, moves):
-        self._state = state # always 4 by 4
+        self._state = state
         self._moves = moves
         self._hValue = None
 
     def getGValue(self):
         # cost of getting to current state (1 per move)
-
         return len(self._moves)
 
     def getHValue(self):
@@ -22,9 +21,11 @@ class Node:
 
         return self.getGValue() + self.getHValue()
 
+    def getMoves(self):
+        return self._moves
+
     def getValuePos(self, value):
         for i in range(4):
             for j in range(4):
                 if self._state[i][j] == value:
                     return i, j
-                    
