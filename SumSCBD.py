da15fd4cb2aea0c701a8fa4b7e8983e7f58fc2a9
HermanLin <hl3213@nyu.edu>
2021-03-29 09:48:52 -0400

    added heuristic class for easy computation

---
+++ SumSCBD.py
@@ -0,0 +1,21 @@
+# Class for easy heuristic calculation
+
+from Node import Node
+
+class SumSCBD:
+
+    def __init__(self, state):
+        self._goalNode = Node(state, None, None, 0)
+
+    def calc_SCBD(self, node):
+        # calculate heuristic using sum of chessboard distances
+        # - chessboard distance = max(horizontal moves, vertical moves)
+
+        sum_SCBD = 0
+        for i in range(16):
+            # find tile positions
+            goalX, goalY = _goalNode.getValuePos(i)
+            currX, currY = node.getValuePos(i)
+            # add the max between hori/vert moves
+            sum_SCBD += max(abs(goalX - currX), abs(goalY, currY))
+        return sum_SCBD
